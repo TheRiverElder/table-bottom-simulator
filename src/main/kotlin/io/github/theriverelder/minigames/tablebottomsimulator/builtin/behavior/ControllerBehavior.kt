5f436b591ff7354d4fcdce1e4f25eb18ddab5fb0
TheRiverElder <765375399@qq.com>
2023-05-11 21:59:34 +0800

    尝试支持TBS

--- src/main/kotlin/io/github/theriverelder/minigames/tablebottomsimulator/builtin/behavior/ControllerBehavior.kt
+++ src/main/kotlin/io/github/theriverelder/minigames/tablebottomsimulator/builtin/behavior/ControllerBehavior.kt
@@ -4,18 +4,28 @@ import io.github.theriverelder.minigames.tablebottomsimulator.gameobject.Behavio
 import io.github.theriverelder.minigames.tablebottomsimulator.gameobject.BehaviorType
 import io.github.theriverelder.minigames.tablebottomsimulator.gameobject.GameObject
 import io.github.theriverelder.minigames.tablebottomsimulator.gameobject.Side
+import io.github.theriverelder.minigames.tablebottomsimulator.user.User
 import kotlinx.serialization.json.*
 
 class ControllerBehavior(type: BehaviorType<ControllerBehavior>, host: GameObject, uid: Int) : BehaviorAdaptor<ControllerBehavior>(type, host, uid) {
 
     var draggable: Boolean = true
+    var controller: User? = null
 
     override fun JsonObjectBuilder.saveData() {
         put("draggable", JsonPrimitive(draggable))
+        put("controller", JsonPrimitive(controller?.uid))
     }
 
     override fun restoreData(data: JsonObject) {
         this.draggable = data["draggable"]?.jsonPrimitive?.booleanOrNull ?: false
+        val controllerUid = data["controller"]?.jsonPrimitive?.intOrNull
+        if (controllerUid != null) {
+            val controller = simulator.users[controllerUid]
+            if (controller != null) {
+                this.controller = controller
+            }
+        }
     }
 
     companion object {
